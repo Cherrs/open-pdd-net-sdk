68af61d5eb4b8bc4e4b7ecc6ab896f2985c594a2
KuriyamaMirai <lovef@outlook.com>
2024-09-20 11:26:11 +0800

    重新生成Order，补充字段

--- src/PddOpenSdk/Models/Request/Ktt/InfoKttPurchaseSampleOrder.cs
+++ src/PddOpenSdk/Models/Request/Ktt/InfoKttPurchaseSampleOrder.cs
@@ -8,39 +8,6 @@ public partial class InfoKttPurchaseSampleOrder
     [JsonPropertyName("request")]
     public RequestModel Request { get; set; }
     public partial class RequestModel
-
-    /* 项目“PddOpenSdk (net8.0)”的未合并的更改
-    在此之前:
-        {
-
-        /// <summary>
-    在此之后:
-        {
-
-            /// <summary>
-    */
-
-    /* 项目“PddOpenSdk (net6.0)”的未合并的更改
-    在此之前:
-        {
-
-        /// <summary>
-    在此之后:
-        {
-
-            /// <summary>
-    */
-
-    /* 项目“PddOpenSdk (netstandard2.0)”的未合并的更改
-    在此之前:
-        {
-
-        /// <summary>
-    在此之后:
-        {
-
-            /// <summary>
-    */
     {
 
         /// <summary>
--- src/PddOpenSdk/Models/Response/Ktt/QuantityKttGoodsIncrResponse.cs
+++ src/PddOpenSdk/Models/Response/Ktt/QuantityKttGoodsIncrResponse.cs
@@ -8,39 +8,6 @@ public partial class QuantityKttGoodsIncrResponse : PddResponseModel
     [JsonPropertyName("ktt_goods_incr_quantity_response")]
     public KttGoodsIncrQuantityResponseResponse KttGoodsIncrQuantityResponse { get; set; }
     public partial class KttGoodsIncrQuantityResponseResponse : PddResponseModel
-
-    /* 项目“PddOpenSdk (net8.0)”的未合并的更改
-    在此之前:
-        {
-
-        /// <summary>
-    在此之后:
-        {
-
-            /// <summary>
-    */
-
-    /* 项目“PddOpenSdk (net6.0)”的未合并的更改
-    在此之前:
-        {
-
-        /// <summary>
-    在此之后:
-        {
-
-            /// <summary>
-    */
-
-    /* 项目“PddOpenSdk (netstandard2.0)”的未合并的更改
-    在此之前:
-        {
-
-        /// <summary>
-    在此之后:
-        {
-
-            /// <summary>
-    */
     {
 
         /// <summary>
