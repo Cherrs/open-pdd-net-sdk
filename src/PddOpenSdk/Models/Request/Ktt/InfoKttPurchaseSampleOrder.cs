4f5c9862e47b0ac1806d1964a31ebf63bf01f7b3
KuriyamaMirai <lovef@outlook.com>
2024-09-20 11:25:48 +0800

    新增快团团相关接口

---
+++ src/PddOpenSdk/Models/Request/Ktt/InfoKttPurchaseSampleOrder.cs
@@ -0,0 +1,55 @@
+namespace PddOpenSdk.Models.Request.Ktt;
+public partial class InfoKttPurchaseSampleOrder
+{
+
+    /// <summary>
+    /// 请求参数
+    /// </summary>
+    [JsonPropertyName("request")]
+    public RequestModel Request { get; set; }
+    public partial class RequestModel
+
+    /* 项目“PddOpenSdk (net8.0)”的未合并的更改
+    在此之前:
+        {
+
+        /// <summary>
+    在此之后:
+        {
+
+            /// <summary>
+    */
+
+    /* 项目“PddOpenSdk (net6.0)”的未合并的更改
+    在此之前:
+        {
+
+        /// <summary>
+    在此之后:
+        {
+
+            /// <summary>
+    */
+
+    /* 项目“PddOpenSdk (netstandard2.0)”的未合并的更改
+    在此之前:
+        {
+
+        /// <summary>
+    在此之后:
+        {
+
+            /// <summary>
+    */
+    {
+
+        /// <summary>
+        /// 订单号
+        /// </summary>
+        [JsonPropertyName("order_sn")]
+        public string OrderSn { get; set; }
+
+    }
+
+}
+
---
+++ src/PddOpenSdk/Models/Response/Ktt/QuantityKttGoodsIncrResponse.cs
@@ -0,0 +1,55 @@
+namespace PddOpenSdk.Models.Response.Ktt;
+public partial class QuantityKttGoodsIncrResponse : PddResponseModel
+{
+
+    /// <summary>
+    /// 结果
+    /// </summary>
+    [JsonPropertyName("ktt_goods_incr_quantity_response")]
+    public KttGoodsIncrQuantityResponseResponse KttGoodsIncrQuantityResponse { get; set; }
+    public partial class KttGoodsIncrQuantityResponseResponse : PddResponseModel
+
+    /* 项目“PddOpenSdk (net8.0)”的未合并的更改
+    在此之前:
+        {
+
+        /// <summary>
+    在此之后:
+        {
+
+            /// <summary>
+    */
+
+    /* 项目“PddOpenSdk (net6.0)”的未合并的更改
+    在此之前:
+        {
+
+        /// <summary>
+    在此之后:
+        {
+
+            /// <summary>
+    */
+
+    /* 项目“PddOpenSdk (netstandard2.0)”的未合并的更改
+    在此之前:
+        {
+
+        /// <summary>
+    在此之后:
+        {
+
+            /// <summary>
+    */
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonPropertyName("success")]
+        public bool? Success { get; set; }
+
+    }
+
+}
+
